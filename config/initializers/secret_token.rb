80376aa4b020e488b6c69e31b7bad6566bbb4272
Zainul Shah <zan2434@gmail.com>
2014-04-05 16:52:53 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareDrive::Application.config.secret_token = '9c8301f8ae7d35913d2606b95512b7a23e7c5c7631a29459cd44cffe6f0c41a66d821be49d6fcd0788c5891aab61d12ac5812b101ba288bc8d2657c301d49a9f'
