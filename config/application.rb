bd8680de72d6f3a9c36cc30d813052ae3a10b263
Adam Syed <suedadam@gmail.com>
2014-04-05 23:38:52 -0700

    Themed Login and Sign-Up screen.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module ShareDrive
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
+      g.orm :active_record
+ 
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       
